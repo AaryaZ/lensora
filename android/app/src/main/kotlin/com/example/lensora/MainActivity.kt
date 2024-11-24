c916b826e399d3313c0409b64dea8d8213112a08
Aarya Zunjarrao <aaryazunjarrao@gmail.com>
2024-11-24 23:43:41 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/lensora/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.lensora
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/home.dart
@@ -0,0 +1,123 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  final double hp = 30;
+  final double vp = 10;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Colors.grey[200]!,
+            Colors.grey[400]!,
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+      child: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: hp, vertical: vp),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Icon(
+                    Icons.home_rounded,
+                    size: 40,
+                    color: Colors.grey[800],
+                  ),
+                  Icon(
+                    Icons.devices,
+                    size: 40,
+                    color: Colors.grey[800],
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 20),
+            Padding(
+              padding: EdgeInsets.symmetric(vertical: vp, horizontal: hp),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text("Welcome To",
+                      style: TextStyle(fontSize: 25, color: Colors.grey[800])),
+                  Text(
+                    "LENSORA",
+                    style: GoogleFonts.bebasNeue(fontSize: 64),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: hp),
+              child: Divider(
+                color: Colors.grey[400],
+                thickness: 1,
+              ),
+            ),
+            const SizedBox(
+              height: 25,
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: hp),
+              child: Text(
+                "Device Handle",
+                style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.grey[800]),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: hp, vertical: 10),
+              child: TextField(
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Colors.grey[200],
+                  border: InputBorder.none,
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12.0),
+                    borderSide: BorderSide(
+                      color: Colors.white,
+                      width: 1,
+                    ),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12.0),
+                    borderSide: BorderSide(
+                      color: Colors.transparent,
+                      width: 1.0,
+                    ),
+                  ),
+                  hintText: "Enter device name",
+                  hintStyle: TextStyle(
+                      color: Colors.grey[700], fontSize: 18), // Hint text style
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    ));
+  }
+}
